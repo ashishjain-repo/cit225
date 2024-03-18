cdce9361de4cd5e33309e888b931146b90e7987a
Ashish Jain <ashishjain99@yahoo.com>
2024-03-18 10:37:24 -0600

    First inserts work work on next

--- project/inserts.sql
+++ project/inserts.sql
@@ -18,3 +18,15 @@ VALUES
 ((SELECT continent_id FROM continent WHERE continent_code = 'AS'),'India','IN'),
 ((SELECT continent_id FROM continent WHERE continent_code = 'EU'),'England','EN'),
 ((SELECT continent_id FROM continent WHERE continent_code = 'AF'),'South Africa','SA');
+
+INSERT INTO author
+(
+    country_id
+,   author_fname
+,   author_mname
+,   author_lname
+,   author_dob
+,   author_dod
+)
+VALUES
+((SELECT country_id FROM country WHERE ))
