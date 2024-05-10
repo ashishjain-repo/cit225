555a5ca16e6caff8a48f26e99a7bf25a98bf8b5c
ashishjain-repo <ashishjain99@yahoo.com>
2024-05-10 09:49:51 -0600

    ADded more files for TA work

---
+++ TA/prophet-insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO prophet
+(
+    first_name
+,   middle_name
+,   last_name
+,   prophet_order
+)
+VALUES
+    ('Joseph',NULL,'Smith',1)
+,   ('Brigham', NULL, 'Young', 2)
+,   ('John', 'Taylor', 'Taylor', 3)
+,   ('Wilford', NULL, 'Woodruff', 4)
+,   ('Lorenzo', 'Snow', 'Snow', 5)
+,   ('Joseph', 'F.', 'Smith', 6)
+,   ('Heber', 'J.', 'Grant', 7)
+,   ('George', 'Albert', 'Smith', 8)
+,   ('David', 'O.', 'McKay', 9)
+,   ('Joseph', 'Fielding', 'Smith', 10)
+,   ('Harold', 'B.', 'Lee', 11)
+,   ('Spencer', 'W.', 'Kimball', 12)
+,   ('Ezra', 'Taft', 'Benson', 13)
+,   ('Howard', 'W.', 'Hunter', 14)
+,   ('Gordon', 'B.', 'Hinckley', 15)
+,   ('Thomas', 'S.', 'Monson', 16)
+,   ('Russell', 'M.', 'Nelson', 17);
